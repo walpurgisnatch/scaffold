eaf38019b53cc2aa002b4d0d474c9196da785189
Walpurgisnatch <Kawemirs@gmail.com>
2022-06-14 12:34:54 +0700

    fileworks utils

--- src/utils.lisp
+++ src/utils.lisp
@@ -13,5 +13,30 @@
 (defmacro concat (string &rest words)
   `(setf ,string (concatenate 'string ,string ,@words)))
 
+(defun directory-pathname-p  (p)
+    (and
+     (not (component-present-p (pathname-name p)))
+     (not (component-present-p (pathname-type p)))
+     p))
 
+(defun pathname-as-directory (name)
+    (let ((pathname (pathname name)))
+        (when (wild-pathname-p pathname)
+            (error "Can't reliably convert wild pathnames."))
+        (if (not (directory-pathname-p name))
+            (make-pathname
+             :directory (append (or (pathname-directory pathname) (list :relative))
+                                (list (file-namestring pathname)))
+             :name      nil
+             :type      nil
+             :defaults pathname)
+            pathname)))
 
+(defun mkdir (dir &optional parent)
+    (namestring (ensure-directories-exist
+                 (if parent
+                     (merge-with-dir dir parent)
+                     (pathname-as-directory dir)))))
+
+(defun merge-with-dir (child parent)
+    (merge-pathnames child (pathname-as-directory parent)))
