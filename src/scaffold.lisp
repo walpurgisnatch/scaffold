c79b8a7d06c2f65ef87f1605811f0a56ddd8814f
Walpurgisnatch <Kawemirs@gmail.com>
2022-06-22 16:11:06 +0700

    setting bindings

--- src/scaffold.lisp
+++ src/scaffold.lisp
@@ -7,6 +7,7 @@
 (in-package :scaffold)
 
 (defparameter *specials* '(("#/" . in-file) ("#:" . set-binds)))
+(defparameter *args* nil)
 (defparameter *binds* nil)
 (defparameter *shapers* '(#\: #\-))
 (defvar *file* nil)
@@ -28,11 +29,27 @@
     (mkdir (relative-path (upper-directory file))))
   (setf *file* (relative-path file)))
 
-(defun set-binds (binds)
-  (setf *binds* (read-from-string binds)))
+(defun set-binds (binds args &optional type result)
+  (let ((bind (car binds))
+        (arg (car args)))
+    (cond ((null arg)
+           result)
+          ((string-starts-with bind "&")
+           (set-binds (cdr binds) args type (acons (subseq bind 1) nil result)))
+          ((string-starts-with arg "-")
+           (set-binds binds (cdr args) (subseq arg 1) result))
+          ((null type)
+           (set-binds (cdr binds) (cdr args) nil (acons bind arg result)))
+          ((null (assoc type result :test #'string=))
+           (format t "missing arguments"))
+          (t (nconc (assoc type result :test #'string=) (list arg))
+             (set-binds binds (cdr args) type result)))))
 
 (defun shaper (char)
   (some #'(lambda (c) (char= char c)) *shapers*))
+
+(defun brake-words (line)
+  (remove-if #'(lambda (x) (string= x "")) (cl-ppcre:all-matches-as-strings "[^\\s]*" line)))
 
 (defun scaffold (string)
   (let ((result ""))
